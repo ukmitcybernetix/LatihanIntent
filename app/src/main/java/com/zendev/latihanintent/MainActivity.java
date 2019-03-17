e10da6effc7aae46b01a704907fa36dc30a04c0f
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-03-17 14:21:43 +0700

    Penambahan Activity

--- app/src/main/java/com/zendev/latihanintent/MainActivity.java
+++ app/src/main/java/com/zendev/latihanintent/MainActivity.java
@@ -1,13 +1,30 @@
 package com.zendev.latihanintent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_activity_login);
+        btnLogin.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_activity_login:
+                break;
+        }
     }
 }
